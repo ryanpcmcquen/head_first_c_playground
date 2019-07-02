5e7e87e35e5a46225260ac3586f7ca924f36dd7a
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-02 13:31:26 -0700

    Count all the cards!
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- cards/cards.c
+++ cards/cards.c
@@ -8,33 +8,40 @@ Take it all at face value.
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
+    int count = 0;
 
-    if (card_name[0] == 'K') {
+    while (card_name[0] != 'X') {
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
 
-        val = 10;
-    } else if (card_name[0] == 'Q') {
-        val = 10;
-    } else if (card_name[0] == 'J') {
-        val = 10;
-    } else if (card_name[0] == 'A') {
-        val = 11;
-    } else {
-        val = atoi(card_name);
+        switch (card_name[0]) {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        case 'X':
+            continue;
+
+        default:
+            val = atoi(card_name);
+            if ((val < 1) || (val > 10)) {
+                puts("I don't understand that value!");
+                continue;
+            }
+        }
+
+        /* Check if the value is 3 to 6: */
+        if (val > 2 && val < 7) {
+            count++;
+        } else if (val == 10) {
+            count--;
+        }
+        printf("Current count: %i\n", count);
     }
-
-    /* Check if the value is 3 to 6: */
-    if (val >= 3 && val <= 6) {
-        puts("Count has gone up.");
-    } else if (val == 10) {
-        /* Otherwise, check if the card was 10, J, Q, or K. */
-        puts("Count has gone down.");
-    }
-    return 0;
-
-    printf("The card value is: %i\n", val);
-
     return 0;
 }
