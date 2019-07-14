6635f3bba996f23bb55bb0d07c81690fab85e671
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-14 16:30:58 -0700

    Add beginning jukebox code.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

---
+++ chapter_02/jukebox/jukebox.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+char tracks[][80] = {
+    "I left my heart in Harvard Med School",
+    "Newark, Newark - a wonderful town",
+    "Dancing with a Dork",
+    "From here to maternity",
+    "The girl from Iwo Jima"
+};
+
+int main()
+{
+    puts(strstr("dysfunctional", "fyn"));
+}
