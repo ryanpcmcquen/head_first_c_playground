29ab61caef51e74ba991e3c11c3ae7a451dd797f
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-15 14:40:30 -0700

    Page 103.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- chapter_02/print_reverse/print_reverse.c
+++ chapter_02/print_reverse/print_reverse.c
@@ -1,9 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
 void print_reverse(char* s)
 {
     size_t len = strlen(s);
+    char* t = s + len - 1;
+
+    while (t >= s) {
+        printf("%c", *t);
+        t = t - 1;
+    }
+    puts("");
 }
 
 int main()
 {
-    print_reverse();
+    print_reverse("foo");
 }
