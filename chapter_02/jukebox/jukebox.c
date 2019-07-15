00998467f43112148140884166ca5c060b737873
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-14 17:13:47 -0700

    Jukebox code works!!
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- chapter_02/jukebox/jukebox.c
+++ chapter_02/jukebox/jukebox.c
@@ -6,10 +6,25 @@ char tracks[][80] = {
     "Newark, Newark - a wonderful town",
     "Dancing with a Dork",
     "From here to maternity",
-    "The girl from Iwo Jima"
+    "The girl from Iwo Jima",
 };
+
+void find_track(char search_for[])
+{
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (strstr(tracks[i], search_for)) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
+}
 
 int main()
 {
-    puts(strstr("dysfunctional", "fyn"));
+    char search_for[80];
+    printf("Search for: ");
+    // https://stackoverflow.com/questions/21318269/bug-in-jukebox-code-head-first-c#21318336
+    scanf("%79s", search_for);
+    find_track(search_for);
+    return 0;
 }
