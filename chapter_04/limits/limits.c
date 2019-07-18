0db1ade7cb863f390c7375ce353cac7d4835b8a0
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-18 16:03:01 -0700

    Page 175.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- chapter_04/limits/limits.c
+++ chapter_04/limits/limits.c
@@ -6,11 +6,11 @@ int main()
 {
     printf("The value of INT_MAX is: %i\n", INT_MAX);
     printf("The value of INT_MIN is: %i\n", INT_MIN);
-    printf("An int takes %z bytes.\n", sizeof(int));
+    printf("An int takes %i bytes.\n", sizeof(int));
 
     printf("The value of FLT_MAX is: %i\n", FLT_MAX);
     printf("The value of FLT_MIN is: %i\n", FLT_MIN);
-    printf("An float takes %z bytes.\n", sizeof(float));
+    printf("An float takes %i bytes.\n", sizeof(float));
 
     return 0;
 }
