8e218b5ef2eff290637597dc32af8d8007dd4d4a
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-08-19 17:02:55 -0700

    Page 472.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

---
+++ chapter_11/advice/advice.c
@@ -0,0 +1,12 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main(int argc, char* argv[])
+{
+    char* advice[] = {
+        "Take smaller bites\r\n",
+        "Go for the tight jeans. No they do NOT make you look fat.\r\n",
+        "One word: inappropriate\r\n",
+        "Just for today, be honest. Tell your"
+    };
+}
