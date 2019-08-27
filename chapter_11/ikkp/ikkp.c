16555fb47f94af3f27b029f32e0356811f9608f9
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-08-27 07:17:45 -0700

    Page 486.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- chapter_11/ikkp/ikkp.c
+++ chapter_11/ikkp/ikkp.c
@@ -1,11 +1,24 @@
+#define _POSIX_SOURCE
+
 #include <arpa/inet.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+int catch_signal(int sig, void (*handler)(int))
+{
+    struct sigaction action;
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+
+    return sigaction(sig, &action, NULL);
+}
 
 void error(char* msg)
 {
@@ -77,14 +90,49 @@ void handle_shutdown(int sig)
         close(listener_d);
     }
 
-    fprintf(stderr, "Bye!\n", );
+    fprintf(stderr, "Bye!\n");
     exit(0);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-
-    sock = open_listener_socket();
-    say("Knock, knock!");
-    read_in(sock, fgets(stdin, 80), 80);
+    if (catch_signal(SIGINT, handle_shutdown) == -1) {
+        error("Can't set the interrupt handler");
+    }
+    listener_d = open_listener_socket();
+    bind_to_port(listener_d, 30000);
+    if (listen(listener_d, 10) == -1) {
+        error("Can't listen.");
+    }
+    struct sockaddr_storage client_addr;
+    unsigned int address_size = sizeof(client_addr);
+    puts("Waiting for connection");
+    char buf[255];
+    while (1) {
+        int connect_d = accept(listener_d, (struct sockaddr*)&client_addr, &address_size);
+        if (connect_d == -1) {
+            error("Can't open secondary socket.");
+        }
+        if (
+            say(
+                connect_d,
+                "Internet Knock-Knock Protocol Server\r\nVersion 1.0\r\nKnock! Knock!\r\n> ")
+            != -1) {
+            read_in(connect_d, buf, sizeof(buf));
+            if (strncasecmp("Who's there?", buf, 12)) {
+                say(connect_d, "You should say 'Who's there?'!");
+            } else {
+                if (say(connect_d, "Oscar\r\n> ") != -1) {
+                    read_in(connect_d, buf, sizeof(buf));
+                    if (strncasecmp("Oscar who?", buf, 10)) {
+                        say(connect_d, "You should say 'Oscar who?'!\r\n");
+                    } else {
+                        say(connect_d, "Oscar silly question, you get a silly answer.\r\n");
+                    }
+                }
+            }
+        }
+        close(connect_d);
+    }
+    return 0;
 }
