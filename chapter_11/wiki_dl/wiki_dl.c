a8747d4dc905bc5d126ef7a117c77a3093520d81
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-09-02 13:24:28 -0700

    Page 513.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- chapter_11/wiki_dl/wiki_dl.c
+++ chapter_11/wiki_dl/wiki_dl.c
@@ -1,10 +1,16 @@
-#ifdef __unix__
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#else
+#ifdef _WIN64
+
 #include <io.h>
 #include <winsock2.h>
+#define strncasecmp(x, y, z) _strnicmp(x, y, z)
+
+#else
+
+#include <arpa/inet.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #endif
 
 #define _POSIX_SOURCE
@@ -16,12 +22,74 @@
 #include <stdlib.h>
 #include <string.h>
 
-// #include <strings.h>
-// ^^ That is replaced by this:
-#define strncasecmp(x, y, z) _strnicmp(x, y, z)
-
 void error(char* msg)
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(1);
+}
+
+int open_socket(char* host, char* port)
+{
+    struct addrinfo* res;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(host, port, &hints, &res) == -1) {
+        error("Can't resolve the address.");
+    }
+
+    int d_sock = socket(
+        res->ai_family,
+        res->ai_socktype,
+        res->ai_protocol);
+    if (d_sock == -1) {
+        error("Can't open socket.");
+    }
+    int c = connect(d_sock, res->ai_addr, res->ai_addrlen);
+    freeaddrinfo(res);
+
+    if (c == -1) {
+        error("Can't connect to socket.");
+    }
+    return d_sock;
+}
+
+int say(int socket, char* s)
+{
+    int result = send(socket, s, strlen(s), 0);
+    if (result == -1) {
+        fprintf(stderr, "%s: %s\n", "Error talking to the server.", strerror(errno));
+    }
+
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+    int d_sock;
+    d_sock = open_socket("https://en.wikipedia.org", "80");
+    char buf[255];
+
+    sprintf(buf, "GET /wiki/%s http/1.1\r\n", argv[1]);
+    say(d_sock, buf);
+
+    say(d_sock, "Host: https://en.wikipedia.org\r\n\r\n");
+    char rec[256];
+
+    int bytesRcvd = recv(d_sock, rec, 255, 0);
+
+    while (bytesRcvd) {
+        if (bytesRcvd == -1) {
+            error("Can't read from the server.");
+        }
+
+        rec[bytesRcvd] = '\0';
+        printf("%s", rec);
+        bytesRcvd = recv(d_sock, rec, 255, 0);
+    }
+
+    close(d_sock);
+    return 0;
 }
