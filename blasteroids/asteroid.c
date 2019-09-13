b87930b568565dc3c9a0274acdac532175f2a112
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-09-13 15:49:11 -0700

    Trying to get the asteroid to move.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- blasteroids/asteroid.c
+++ blasteroids/asteroid.c
@@ -1,44 +1,88 @@
 // #include "renderer.c"
+#include "config.h"
 #include <SDL2/SDL.h>
 
-typedef struct Asteroid {
-    float sx;
-    float sy;
-    float heading;
-    float twist;
-    float speed;
-    float rot_velocity;
-    float scale;
-    int gone;
-    SDL_Color color;
-} Asteroid;
-
-void draw_asteroid(SDL_Renderer** rndr)
+void draw_asteroid(SDL_Renderer** renderer, SDL_Event* event, int x, int y)
 {
-    // Asteroid a;
-    // SDL_Renderer* rndr = rend();
-    // ALLEGRO_TRANSFORM transform;
-    // al_identity_transform(&transform);
-    // al_rotate_transform(&transform, DEGREES(a->heading));
-    // al_translate_transform(&transform, a->sx, a->sy);
-    // al_use_transform(&transform);
-    SDL_SetRenderDrawColor(*rndr, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(*rndr);
-    SDL_SetRenderDrawColor(*rndr, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_SetRenderDrawColor(*renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(*renderer);
+    SDL_SetRenderDrawColor(*renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
 
-    SDL_RenderDrawLine(*rndr, -20, 20, -25, 5);
-    SDL_RenderDrawLine(*rndr, -25, 5, -25, -10);
-    SDL_RenderDrawLine(*rndr, -25, -10, -5, -10);
-    SDL_RenderDrawLine(*rndr, -5, -10, -10, -20);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) - 20 + x,
+        (SCREEN_HEIGHT / 2) + 20 + y,
+        (SCREEN_WIDTH / 2) - 25 - x,
+        (SCREEN_HEIGHT / 2) + 5 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) - 25 + x,
+        (SCREEN_HEIGHT / 2) + 5 + y,
+        (SCREEN_WIDTH / 2) - 25 - x,
+        (SCREEN_HEIGHT / 2) - 10 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) - 25 + x,
+        (SCREEN_HEIGHT / 2) - 10 + y,
+        (SCREEN_WIDTH / 2) - 5 - x,
+        (SCREEN_HEIGHT / 2) - 10 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) - 5 + x,
+        (SCREEN_HEIGHT / 2) - 10 + y,
+        (SCREEN_WIDTH / 2) - 10 - x,
+        (SCREEN_HEIGHT / 2) - 20 - y);
 
-    SDL_RenderDrawLine(*rndr, -10, -20, 5, -20);
-    SDL_RenderDrawLine(*rndr, 5, -20, 20, -10);
-    SDL_RenderDrawLine(*rndr, 20, -10, 20, -5);
-    SDL_RenderDrawLine(*rndr, 20, -5, 0, 0);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) - 10 + x,
+        (SCREEN_HEIGHT / 2) - 20 + y,
+        (SCREEN_WIDTH / 2) + 5 - x,
+        (SCREEN_HEIGHT / 2) - 20 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) + 5 + x,
+        (SCREEN_HEIGHT / 2) - 20 + y,
+        (SCREEN_WIDTH / 2) + 20 - x,
+        (SCREEN_HEIGHT / 2) - 10 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) + 20 + x,
+        (SCREEN_HEIGHT / 2) - 10 + y,
+        (SCREEN_WIDTH / 2) + 20 - x,
+        (SCREEN_HEIGHT / 2) - 5 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) + 20 + x,
+        (SCREEN_HEIGHT / 2) - 5 + y,
+        (SCREEN_WIDTH / 2) + 0 - x,
+        (SCREEN_HEIGHT / 2) + 0 - y);
 
-    SDL_RenderDrawLine(*rndr, 0, 0, 20, 10);
-    SDL_RenderDrawLine(*rndr, 20, 10, 10, 20);
-    SDL_RenderDrawLine(*rndr, 10, 20, 0, 15);
-    SDL_RenderDrawLine(*rndr, 0, 15, -20, 20);
-    SDL_RenderPresent(*rndr);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) + 0 + x,
+        (SCREEN_HEIGHT / 2) + 0 + y,
+        (SCREEN_WIDTH / 2) + 20 - x,
+        (SCREEN_HEIGHT / 2) + 10 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) + 20 + x,
+        (SCREEN_HEIGHT / 2) + 10 + y,
+        (SCREEN_WIDTH / 2) + 10 - x,
+        (SCREEN_HEIGHT / 2) + 20 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) + 10 + x,
+        (SCREEN_HEIGHT / 2) + 20 + y,
+        (SCREEN_WIDTH / 2) + 0 - x,
+        (SCREEN_HEIGHT / 2) + 15 - y);
+    SDL_RenderDrawLine(
+        *renderer,
+        (SCREEN_WIDTH / 2) + 0 + x,
+        (SCREEN_HEIGHT / 2) + 15 + y,
+        (SCREEN_WIDTH / 2) - 20 - x,
+        (SCREEN_HEIGHT / 2) + 20 - y);
+
+    SDL_RenderPresent(*renderer);
+    printf("SDL error: %s\n", SDL_GetError());
 }
--- blasteroids/blasteroids.c
+++ blasteroids/blasteroids.c
@@ -1,38 +1,30 @@
 #include "asteroid.c"
+#include "config.h"
 // #include "blast.c"
 // #include "spaceship.c"
 #include <SDL2/SDL.h>
 
 int main(int argc, char* argv[])
 {
-    // draw_asteroid();
     if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+        int x = 0;
+        int y = 0;
 
         SDL_Window* window = NULL;
         SDL_Renderer* renderer = NULL;
 
-        if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0) {
+        if (SDL_CreateWindowAndRenderer((int)SCREEN_WIDTH, (int)SCREEN_HEIGHT, 0, &window, &renderer) == 0) {
             SDL_bool done = SDL_FALSE;
-            int x = 0;
-            int y = 0;
             while (!done) {
-                draw_asteroid(&renderer);
                 SDL_Event event;
-                //             SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-                //             SDL_RenderClear(renderer);
-
-                //             SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-                //             SDL_RenderDrawLine(renderer, 320 + x, 200, 300, 240);
-                //             SDL_RenderDrawLine(renderer, 300 + x, 240, 340, 240);
-                //             SDL_RenderDrawLine(renderer, 340 + x, 240, 320, 200);
-
-                //             SDL_RenderPresent(renderer);
+                draw_asteroid(&renderer, &event, x, y);
 
                 while (SDL_PollEvent(&event)) {
                     switch (event.type) {
                     case SDL_QUIT:
                         done = SDL_TRUE;
                         break;
+
                     case SDL_KEYDOWN:
                         switch (event.key.keysym.sym) {
                         case SDLK_LEFT:
@@ -53,15 +45,17 @@ int main(int argc, char* argv[])
                         }
                     }
                 }
+
+                if (renderer) {
+                    SDL_DestroyRenderer(renderer);
+                }
+                if (window) {
+                    SDL_DestroyWindow(window);
+                }
             }
-            if (renderer) {
-                SDL_DestroyRenderer(renderer);
-            }
-            if (window) {
-                SDL_DestroyWindow(window);
-            }
+            SDL_Quit();
         }
-        SDL_Quit();
     }
+
     return 0;
 }
